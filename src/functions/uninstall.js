943581c736ef9b30fd5e983e2f341d6c111d0df4
paige <88659700+paishee@users.noreply.github.com>
2024-01-18 13:49:33 -0600

    Update uninstall.js

--- src/functions/uninstall.js
+++ src/functions/uninstall.js
@@ -15,7 +15,7 @@ function PiceUninstall (pkg="", args={}) {
 
 
     // sync executor
-    if (args.sync || !args.async) com.executor = this.exec(comStr);
+    if (args.sync || !args.async) com.__executor = this.exec(comStr);
 
 
     // async executor
@@ -24,15 +24,15 @@ function PiceUninstall (pkg="", args={}) {
 
         setImmediate( () => {
             try {
-                com.executor = this.execAsync(comStr)
+                com.__executor = this.execAsync(comStr)
                 
-                com.executor.then( () => {
+                com.__executor.then( () => {
                     if (com.__listeners.then.length > 0) {
                         com.__listeners.then.forEach( f => f(com) );
                     }
                 });
 
-                com.executor.finally( () => {
+                com.__executor.finally( () => {
                     if (com.__listeners.finally.length > 0) {
                         com.__listeners.finally.forEach( f => f(com) );
                     } 
