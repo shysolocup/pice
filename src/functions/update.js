e8e160fc35f17fd264ed7f0bb6b7e96db07cb953
paige <88659700+paishee@users.noreply.github.com>
2024-01-18 13:49:57 -0600

    Update update.js

--- src/functions/update.js
+++ src/functions/update.js
@@ -9,7 +9,7 @@ function PiceUpdate(pkg="", args={}) {
 
 
     // sync executor
-    if (args.sync || !args.async) com.executor = this.exec(comStr);
+    if (args.sync || !args.async) com.__executor = this.exec(comStr);
 
 
     // async executor
@@ -18,16 +18,16 @@ function PiceUpdate(pkg="", args={}) {
 
         setImmediate( () => {
             try {
-                com.executor = this.execAsync(comStr)
+                com.__executor = this.execAsync(comStr)
                 
-                com.executor.then( () => {
+                com.__executor.then( () => {
                     if (com.__listeners.then.length > 0) {
                         let req = require(pkg);
                         com.__listeners.then.forEach( f => f(req, com) );
                     }
                 });
 
-                com.executor.finally( () => {
+                com.__executor.finally( () => {
                     if (com.__listeners.finally.length > 0) {
                         let req = require(pkg);
                         com.__listeners.finally.forEach( f => f(req, com) );
