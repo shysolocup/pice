6386714bc12a1ed503922af6e7ffe2977766003c
paige <88659700+paishee@users.noreply.github.com>
2024-01-18 12:23:05 -0600

    Update uninstall.js

--- src/functions/uninstall.js
+++ src/functions/uninstall.js
@@ -31,11 +31,16 @@ function PepmUninstall (pkg="", args={}) {
                 
                 com.executor.then( () => {
                     if (com.__listeners.then.length > 0) {
-                        (async () => {
-                            let req = require(pkg);
-                            com.__listeners.then.forEach( f => f(req, com) );
-                        })()
+                        let req = require(pkg);
+                        com.__listeners.then.forEach( f => f(req, com) );
                     }
+                });
+
+                com.executor.finally( () => {
+                    if (com.__listeners.finally.length > 0) {
+                        let req = require(pkg);
+                        com.__listeners.finally.forEach( f => f(req, com) );
+                    } 
                 });
             } catch(e) {
                 if (com.__listeners.catch.length > 0) {
@@ -47,12 +52,19 @@ function PepmUninstall (pkg="", args={}) {
     }
 
 
-    // then
+    // then and finally
     if (!com.async) setImmediate( () => {
         if (com.__listeners.then.length > 0) {
             (async () => {
                 let req = require(pkg);
                 com.__listeners.then.forEach( f => f(req, com) );
+            })()
+        }
+
+        if (com.__listeners.finally.length > 0) {
+            (async () => {
+                let req = require(pkg);
+                com.__listeners.finally.forEach( f => f(req, com) );
             })()
         }
     });
