300402dbf41b52f05ce72b55804f8d1180b9da93
paige <88659700+paishee@users.noreply.github.com>
2024-01-18 11:11:18 -0600

    Create compile.js

---
+++ src/compile.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+module.exports = ((d) => {
+    let dir = `${__dirname}/${d}`;
+    let files = fs.readdirSync(dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));  
+    files.forEach( (file) => {
+        require(`${dir}/${file}`);
+    });
+})
